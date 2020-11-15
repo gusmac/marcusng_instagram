389f7ab3f7770b32aabc17bdf0fc1a2122eee457
gusmac <gusmac@gmail.com>
2020-11-15 13:45:31 +0800

    added bottom navbar and screens

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:marcusng_instagram/screens/activity_screen.dart';
+import 'package:marcusng_instagram/screens/create_post_screen.dart';
+import 'package:marcusng_instagram/screens/feed_screen.dart';
+import 'package:marcusng_instagram/screens/profile_screen.dart';
+import 'package:marcusng_instagram/screens/search_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,6 +12,16 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentTab = 0;
+  PageController _pageController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    _pageController = PageController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +35,67 @@ class _HomeScreenState extends State<HomeScreen> {
             fontSize: 35.0,
           ),
         ),
+      ),
+      body: PageView(
+        controller: _pageController,
+        children: [
+          FeedScreen(),
+          SearchScreen(),
+          CreatePostScreen(),
+          ActivityScreen(),
+          ProfileScreen(),
+        ],
+        onPageChanged: (int index) {
+          setState(() {
+            _currentTab = index;
+          });
+        },
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        currentIndex: _currentTab,
+        onTap: (int index) {
+          setState(() {
+            _currentTab = index;
+          });
+          _pageController.animateToPage(
+            index,
+            duration: Duration(milliseconds: 200),
+            curve: Curves.easeIn,
+          );
+        },
+        activeColor: Colors.black,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              size: 32.0,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              size: 32.0,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.photo_camera,
+              size: 32.0,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.notifications,
+              size: 32.0,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.account_circle,
+              size: 32.0,
+            ),
+          ),
+        ],
       ),
     );
   }
